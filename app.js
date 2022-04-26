cbdd4607cbbcbdd61899170b4cd239ed4ac01c4e
John Idenyi <johnidenyi2001@gmail.com>
2022-04-26 12:47:55 -0400

    Added errorMsg

--- app.js
+++ app.js
@@ -1,57 +1,29 @@
 const form = document.querySelector("#my-form");
 const emailInput = document.querySelector("#email");
+const errorMsg = document.querySelector(".error-msg");
 
 form.addEventListener("submit", validateForm);
 
 function validateForm(e) {
     if (emailInput.value === "") {
-        showError("Please enter a valid email address");
+        showInfo("Please enter a valid email address", "red");
     } else {
-        showInfo("Thank you for subscribing to our newsletter!");
+        showInfo("Thank you for subscribing to our newsletter!", "green");
     }
 
     e.preventDefault();
 }
 
-// Show Error
-function showError(msg) {
-    const div = document.querySelector(".form");
+// Show Info
+function showInfo(msg, color) {
+    errorMsg.textContent = msg;
+    errorMsg.style.color = color;
 
-    // Create Element
-    const p = document.createElement("p");
-    p.className = "error-msg";
-    p.style.color = "red";
-    // Create text and append to div
-    const text = document.createTextNode(msg);
-    p.appendChild(text);
-
-    div.insertBefore(p, form);
-
-    // Clear error after 3 secs
-    setTimeout(clearMessage, 4000);
-}
-
-
-// show Info
-function showInfo(msg) {
-    const div = document.querySelector(".form");
-
-    // Create Element
-    const p = document.createElement("p");
-    p.className = "error-msg";
-    p.style.color = "green";
-    // Create text and append to div
-    const text = document.createTextNode(msg);
-    // text.style.color = "red";
-    p.appendChild(text);
-
-    div.insertBefore(p, form);
-
-    // Clear error after 3 secs
-    setTimeout(clearMessage, 4000);
+    // Clear message after 3 secs
+    setTimeout(clearMessage, 3000);
 }
 
 // Clear message
 function clearMessage() {
-    document.querySelector(".error-msg").remove();
+    document.querySelector(".error-msg").textContent = "";
 }
